18524b1514d70a529f6d14b979063d6049b96a9a
SiAvMaAnR <siavmaanr@gmail.com>
2024-10-08 01:09:56 +0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import { ConfigService } from '@nestjs/config';
+import { AppConfig } from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
 
   const configService = app.get(ConfigService);
-  const appPort = configService.get<number>('app.port');
+  const { port, allowedOrigins } = configService.get<AppConfig>('app');
 
-  await app.listen(appPort);
+  app.enableCors({
+    origin: allowedOrigins,
+    credentials: true,
+  });
+
+  await app.listen(port);
 }
 bootstrap();
